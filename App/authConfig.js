1c001f4d69bdd423463993779cfa0a8cc7ddbcdc
Yap Wei Jie <weijieyap16@gmail.com>
2022-07-10 00:54:25 +0800

    Initial commit

---
+++ App/authConfig.js
@@ -0,0 +1,69 @@
+/**
+ * Configuration object to be passed to MSAL instance on creation. 
+ * For a full list of MSAL.js configuration parameters, visit:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md 
+ */
+
+const msalConfig = {
+  auth: {
+    clientId: "9fdfc925-924c-4e70-abda-d3e81b3c3e9b", // This is the ONLY mandatory field that you need to supply.
+    authority: "https://login.microsoftonline.com/db992bae-4cb3-4086-8c91-55255b0c39fe", // Defaults to "https://login.microsoftonline.com/common"
+    redirectUri: "http://localhost:3000", // You must register this URI on Azure Portal/App Registration. Defaults to window.location.href
+    postLogoutRedirectUri: "http://localhost:3000/signout", // Simply remove this line if you would like navigate to index page after logout.
+    navigateToLoginRequestUrl: false, // If "true", will navigate back to the original request location before processing the auth code response.
+  },
+  cache: {
+    cacheLocation: "localStorage", // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO.
+    storeAuthStateInCookie: false, // If you wish to store cache items in cookies as well as browser cache, set this to "true".
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback: (level, message, containsPii) => {
+        if (containsPii) {
+          return;
+        }
+        switch (level) {
+          case msal.LogLevel.Error:
+            console.error(message);
+            return;
+          case msal.LogLevel.Info:
+            console.info(message);
+            return;
+          case msal.LogLevel.Verbose:
+            console.debug(message);
+            return;
+          case msal.LogLevel.Warning:
+            console.warn(message);
+            return;
+        }
+      }
+    }
+  }
+};
+
+/**
+ * Scopes you add here will be prompted for user consent during sign-in.
+ * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
+ * For more information about OIDC scopes, visit: 
+ * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
+ */
+const loginRequest = {
+  scopes: ["openid", "profile"],
+};
+
+/**
+ * An optional silentRequest object can be used to achieve silent SSO
+ * between applications by providing a "login_hint" property.
+ */
+
+// const silentRequest = {
+//   scopes: ["openid", "profile"],
+//   loginHint: "example@domain.net"
+// };
+
+// exporting config object for jest
+if (typeof exports !== 'undefined') {
+  module.exports = {
+    msalConfig: msalConfig,
+  };
+}
